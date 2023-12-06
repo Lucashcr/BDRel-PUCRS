8eb7fc5d28825fc656f4f1b29d933a4b60f62d64
Lucashcr <lucash.rocha@hotmail.com>
2023-12-06 01:58:31 -0300

    Rank das municipios por quantidade de ocorrências de roubo de veículos

--- rank_municipios.py
+++ rank_municipios.py
@@ -9,25 +9,22 @@ engine = sa.create_engine('sqlite:///ocorrencias.db')
 session = sessionmaker(engine)()
 
 query = sa.select(
-    DP.nome.label('DP'),
+    Municipio.municipio.label('Municipio'),
     sa.func.sum(Ocorrencia.qtde).label('Total')
 ).join(
     Ocorrencia,
-    Ocorrencia.codDP == DP.codDP
-).join(
-    Municipio,
     Ocorrencia.codIBGE == Municipio.codIBGE
 ).where(
-    Municipio.regiao == 'Capital'
+    Ocorrencia.ocorrencia == 'roubo_veiculo'
 ).group_by(
-    DP.nome
+    Municipio.municipio
 ).order_by(
-    sa.func.sum(Ocorrencia.qtde).label('Total').desc()
+    sa.func.sum(Ocorrencia.qtde).desc()
 )
 
 result = session.execute(query)
 
-print('  # | DP' + ' '*48 + ' |  qtde')
+print('  # | Municipio' + ' '*41 + ' |  qtde')
 print('-'*65)
 for i, (dp, c) in enumerate(result.fetchall(), start=1):
     print(f'{i:3} | {dp:50} | {c:6}')
